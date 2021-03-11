3703dd00ffcf7fd9baa3d40b11f45f839edcdf4e
kkkewiiiw <73580370+kkkewiiiw@users.noreply.github.com>
2021-03-12 00:18:18 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -18,11 +18,11 @@ z[0,0] = 10 #initial value x
 #variable_y
 z[0,1] = 10 #initial value y
 
-expr1 = lambda x1,y1,x2,y2 : #calculate by "Ito_Abe_method.py"
-expr2 = lambda x1,y1,x2,y2 : #calculate by "Ito_Abe_method.py"
+expr1 = lambda x1,y1,x2,y2 : x1 - x2 + h *(#calculate by "Ito_Abe_method.py")
+expr2 = lambda x1,y1,x2,y2 : y1 - y2 + h * (#calculate by "Ito_Abe_method.py")
 
-df_x = lambda x,y,x2 : #calculate by "partial_differencial.py"
-df_y = lambda x,y,y2 : #calculate by "partial_differencial.py"
+df_x = lambda x,y,x2 : x - x2 + h * (#calculate by "partial_differencial.py")
+df_y = lambda x,y,y2 : y - y2 + h * (#calculate by "partial_differencial.py")
 
 for i in range(N):
 
